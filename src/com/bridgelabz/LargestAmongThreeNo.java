8545271bf177cdd6f2d4664b2b3c615e7ec21ed9
nilofarmujawar <99306976+nilofarmujawar@users.noreply.github.com>
2022-02-17 19:14:02 +0530

    Largest Among Three No Program Is Done.

--- src/com/bridgelabz/LargestAmongThreeNo.java
+++ src/com/bridgelabz/LargestAmongThreeNo.java
@@ -7,8 +7,11 @@ public class LargestAmongThreeNo {
         public static void main(String args[]) {
 
             int num1, num2, num3; // create 3
+                
             System.out.println("Enter three integers: ");
+                
             Scanner in = new Scanner(System.in);
+                
             num1=in.nextInt();
             num2=in.nextInt();
             num3=in.nextInt();
